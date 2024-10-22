e4d7e350e1aeaebe39c8aca66140d721ea2e36c8
nfmmendes <nilson.fmmendes@outlook.com>
2024-10-22 18:47:14 +0200

    Create request endpoints on app.js

--- node/app.js
+++ node/app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3001
+let randomList = []
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 2')
+})
+
+app.put('/request', (req, res) => { 
+  let departure = Math.round(Math.random()*1000);
+  let arrival = Math.round(Math.random()*1000);
+  while(departure === arrival)
+    arrival = Math.random()%1000
+  randomList.push(`Request from ${departure} to ${arrival}`)
+  res.send('')
+})
+
+app.get('/request', (req, res) => {
+  res.send(randomList)
 })
 
 app.listen(port, () => {
